0837dbf16ca1242239fd3bae12da331f17d58eb1
ha-jok <1273447417@qq.com>
2022-03-25 20:52:02 +0800

    docs(all):删除了不必要文件夹

--- 后端/FinalExam/业务题/3G含金量代码/model/user.go
+++
@@ -1,8 +0,0 @@
-package model
-
-//创建和数据表结合的结构体
-type User struct {
-	Username string
-	Password string
-	Money int
-}
--- 国庆预习/后端/leve0/hello.go
+++
@@ -1,9 +0,0 @@
-package main
-import (
-	"fmt"
-	"time"
-)
-func main(){
-	fmt.Println("Hello World!")
-	time.Sleep(5*time.Second)
-}
